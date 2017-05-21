52895a6f0390f8cff2aef3b1dda766921c9c477f
Cdfghglz <cdfghglz@gmail.com>
2017-05-21 23:43:55 +0200

    activity code map loading

--- AJump.cpp
+++ AJump.cpp
@@ -4,23 +4,48 @@
 #include <activityjumperadaptor.h>
 
 ActivityJumper::ActivityJumper(QObject *parent)
-    : QObject(parent)
-{
-    qDebug() << __PRETTY_FUNCTION__;
-    new ActivityJumperAdaptor(this);
-    QDBusConnection dbus = QDBusConnection::sessionBus();
-    dbus.registerObject("/org/kde/ActivityJumper", this);
-    dbus.registerService("org.kde.ActivityJumper");
+		: QObject(parent) {
+	qDebug() << __PRETTY_FUNCTION__;
+	new ActivityJumperAdaptor(this);
+	QDBusConnection dbus = QDBusConnection::sessionBus();
+	dbus.registerObject("/ActivityJumper", this);
+	dbus.registerService("org.kde.ActivityJumper");
+	loadActivityCodeMap();
 }
 
 ActivityJumper::~ActivityJumper() {}
 
-void ActivityJumper::jumpTo(QString destinArg)
-{
-    qDebug() << __PRETTY_FUNCTION__ << "recieved" << destinArg;
+void ActivityJumper::loadActivityCodeMap() {
+	QDBusInterface *activityListInterface = new QDBusInterface("org.kde.ActivityManager",
+												   "/ActivityManager/Activities",
+												   "org.kde.ActivityManager.Activities",
+												   QDBusConnection::sessionBus(),
+												   this);
+
+	QDBusMessage activityListResponse = activityListInterface->call("ListActivities");
+
+	if (activityListResponse.type() == QDBusMessage::ReplyMessage) {
+
+		QListIterator<QString> itr(activityListResponse.arguments().at(0).toStringList());
+
+		while (itr.hasNext()) {
+			QString activityCode = itr.next();
+			QString activityName = activityListInterface->call("ActivityName", activityCode).arguments().at(0).toString();
+			activityCodeMap_.insert(activityName, activityCode);
+		}
+
+	}
+	else if (activityListResponse.type() == QDBusMessage::ErrorMessage) {
+		qDebug() << __PRETTY_FUNCTION__;
+		qDebug() << activityListResponse.errorName();
+	}
+
 }
 
-void ActivityJumper::jumpBack()
-{
-    qDebug() << __PRETTY_FUNCTION__;
+void ActivityJumper::jumpTo(QString destinArg) {
+	qDebug() << __PRETTY_FUNCTION__ << "recieved" << destinArg;
+}
+
+void ActivityJumper::jumpBack() {
+	qDebug() << __PRETTY_FUNCTION__;
 }
--- AJump.hpp
+++ AJump.hpp
@@ -2,19 +2,22 @@
 #define ACTIVITYJUMPER_HH
 
 #include <QObject>
+#include <QMap>
 
 class ActivityJumper: public QObject
 {
     Q_OBJECT;
     Q_CLASSINFO("D-Bus Interface", "org.kde.ActivityJumper");
 
+private:
+QMap<QString, QString> activityCodeMap_;
+	void loadActivityCodeMap();
 public:
     ActivityJumper(QObject *parent);
     virtual ~ActivityJumper();
 
 public slots:
     void jumpTo(QString destinArg);
-
 	void jumpBack();
 
 };
